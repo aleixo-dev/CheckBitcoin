3899038cf97672305923fbd293f2f0c3d735c37e
Aleixo-Dev <nicolasaleixo2020@gmail.com>
2022-09-21 11:50:17 -0300

    config db into di module

--- app/src/main/java/br/com/nicolas/checkbitcoin/di/DiModule.kt
+++ app/src/main/java/br/com/nicolas/checkbitcoin/di/DiModule.kt
@@ -1,8 +1,10 @@
 package br.com.nicolas.checkbitcoin.di
 
+import androidx.room.Room
 import br.com.nicolas.checkbitcoin.data.repository.BitcoinDataSource
 import br.com.nicolas.checkbitcoin.data.repository.BitcoinRepository
 import br.com.nicolas.checkbitcoin.data.service.BitcoinService
+import br.com.nicolas.checkbitcoin.model.local.database.BitcoinDatabase
 import br.com.nicolas.checkbitcoin.ui.home.HomeViewModel
 import br.com.nicolas.checkbitcoin.utils.Constants
 import org.koin.androidx.viewmodel.dsl.viewModel
@@ -18,6 +20,15 @@ val instance = module {
         BitcoinRepository(
             apiService = get()
         )
+    }
+
+    single {
+        Room.databaseBuilder(
+            get(),
+            BitcoinDatabase::class.java,
+            "bitcoin_database"
+        ).build()
+        get<BitcoinDatabase>().bitcoinDao()
     }
 
     viewModel {
